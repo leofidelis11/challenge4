a09f8fc515111b94190c9094c10e22a2b59f4941
DiasLuc <diaslucp@gmail.com>
2025-07-31 17:30:00 -0400

    set test to utilize data-qa IDs

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,9 @@
 describe('Login', () => {
     it('should login successfully', () => {
         cy.visit('http://localhost:4000');
-        cy.get('#username').type('alice');
-        cy.get('#loginForm input#password').click({force: true}).type('password123');
-
-        cy.contains('button', 'Login').click();
-
-        cy.get('#status-text').should('have.text', 'Login successful! Welcome back.');
+        cy.get('[data-qa="username-field"]').click().type('alice');
+        cy.get('[data-qa="password-label"]').click().type('password123');
+        cy.get('[data-qa="login-button"]').click();
+        cy.get('[data-qa="status-text"]').should('have.text', 'Login successful! Welcome back.');
     });
 });
