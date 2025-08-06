a572d18a67265f5fdc2b6363fac70800ef1942ac
CodeIsa <isaoliveira782@gmail.com>
2025-08-06 13:16:51 -0300

    Adding invalid credentials test, Readme and refactoring code

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,41 +1,55 @@
-describe('Login', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:4000');
+describe("Login", () => {
+  it("should login successfully", () => {
+    cy.loginWith("alice", "password123");
+
+    cy.get('[data-qa="status-text"]').should(
+      "have.text",
+      "Login successful! Welcome back."
+    );
+  });
+
+  it("Should fail to login with invalid password", () => {
+    cy.loginWith("alice", "wrongpassword");
+
+    cy.get('[data-qa="status-text"]').should(
+      "have.text",
+      "Invalid username or password"
+    );
+  });
+
+  it("Should fail to login with invalid username", () => {
+    cy.loginWith("invalidUser", "password123");
+    cy.get('[data-qa="status-text"]').should(
+      "have.text",
+      "Invalid username or password"
+    );
+  });
+
+  it("shouldn't allow sign in attempt with empty username field", () => {
+    cy.loginWith("", "password123");
+
+    cy.get('[data-qa="username-field"]').should("have.value", "");
+    cy.get('[data-qa="password-field"]').should("have.value", "password123");
+    // Verify no success message appears (form submission was prevented)
+    cy.get('[data-qa="status-text"]').should("not.be.visible");
+  });
+
+  it("shouldn't allow sign in attempt with empty password field", () => {
+    cy.loginWith("charlie", "");
+
+    cy.get('[data-qa="password-field"]').should("have.value", "");
+    cy.get('[data-qa="status-text"]').should("not.be.visible");
+  });
+
+  it("should return error if account is blocked due too many failed login attempts", () => {
+    cy.loginWith("dave", "wrongpassword");
+
+    Cypress._.times(3, () => {
+      cy.get('[data-qa="login-button"]').click();
     });
-    it('should login successfully', () => {
-        cy.loginWith('alice', 'password123');
-  
-        cy.get('[data-qa="status-text"]').should('have.text', 'Login successful! Welcome back.');
-    });
-
-    it('should return error if password is invalid', () => {
-        cy.loginWith('alice', 'wrongpassword');
-
-        cy.get('[data-qa="status-text"]').should('have.text', 'Invalid username or password');
-    });
-
-    it('shouldn\'t allow sign in attempt with empty username field', () => {        
-        cy.loginWith('', 'password123');
-
-        cy.get('[data-qa="username-field"]').should('have.value', '');
-        cy.get('[data-qa="password-field"]').should('have.value', 'password123');
-        // Verify no success message appears (form submission was prevented)
-        cy.get('[data-qa="status-text"]').should('not.be.visible');
-    });
-
-    it('shouldn\'t allow sign in attempt with empty password field', () => {
-        cy.loginWith('charlie', '');
-
-        cy.get('[data-qa="password-field"]').should('have.value', '');
-        cy.get('[data-qa="status-text"]').should('not.be.visible');
-    });
-[]
-    it('should return error if account is blocked due too many failed login attempts', () => {
-        cy.loginWith('dave', 'wrongpassword');
-
-        Cypress._.times(3, () => {
-            cy.get('[data-qa="login-button"]').click();
-          });
-        cy.get('[data-qa="status-text"]').should('have.text', 'Account is blocked due to too many failed attempts');
-    });
+    cy.get('[data-qa="status-text"]').should(
+      "have.text",
+      "Account is blocked due to too many failed attempts"
+    );
+  });
 });
--- cypress/support/login.js
+++ cypress/support/login.js
@@ -1,16 +1,17 @@
-Cypress.Commands.add('loginWith', (username, password) => {
-    cy.get('[data-qa="username-field"]').as('username');
-    cy.get('[data-qa="password-label"]').as('passwordLabel');
-    cy.get('[data-qa="password-field"]').as('password');
+Cypress.Commands.add("loginWith", (username, password) => {
+  cy.visit("http://localhost:4000");
+  cy.get('[data-qa="username-field"]').as("username");
+  cy.get('[data-qa="password-label"]').as("passwordLabel");
+  cy.get('[data-qa="password-field"]').as("password");
 
-    username.length === 0 
-        ? cy.get('@username').clear() 
-        : cy.get('@username').click().type(username);
+  username.length === 0
+    ? cy.get("@username").clear()
+    : cy.get("@username").click().type(username);
 
-    cy.get('@passwordLabel').click();
-    password.length === 0
-        ? cy.get('@password').clear()
-        : cy.get('@password').click().type(password);
+  cy.get("@passwordLabel").click();
+  password.length === 0
+    ? cy.get("@password").clear()
+    : cy.get("@password").click().type(password);
 
-    cy.get('[data-qa="login-button"]').click();
-})
+  cy.get('[data-qa="login-button"]').click();
+});
