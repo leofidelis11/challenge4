f44def6b544e79855a5de94eb117709f7379667a
CodeIsa <isaoliveira782@gmail.com>
2025-08-06 12:20:24 -0300

    adding new test for invalid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,31 @@
 describe('Login', () => {
-    it('should login successfully', () => {
+    beforeEach(() => {
         cy.visit('http://localhost:4000');
+    });
+    it('should login successfully', () => {
         cy.get('#username').type('alice');
-        cy.get('#loginForm input#password').click({force: true}).type('password123');
+        cy.get('#loginForm input#password').click({ force: true }).type('password123');
 
         cy.contains('button', 'Login').click();
 
         cy.get('#status-text').should('have.text', 'Login successful! Welcome back.');
+    });
+
+    it('Should fail to login with incorrect password', () => {
+        cy.get('#username').type('alice');
+        cy.get('#loginForm input#password').click({ force: true }).type('wrongpassword');
+
+        cy.contains('button', 'Login').click();
+
+        cy.get('#status-text').should('have.text', 'Invalid username or password');
+    });
+
+    it('Should fail to login with incorret username', () => {
+        cy.get('#username').type('invalidUser');
+        cy.get('#loginForm input#password').click({ force: true }).type('password123');
+
+        cy.contains('button', 'Login').click();
+
+        cy.get('#status-text').should('have.text', 'Invalid username or password');
     });
 });
